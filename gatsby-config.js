040a1990e50f6d6941d5e9012a9a145dde4c9d30
mmmommm <ryota1839@gmail.com>
2020-10-23 15:55:39 +0900

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,11 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Dev Blog",
+    description: "Gatsbyで作成したブログサイトです。",
+    author: "Engineer X"
+  },
+    /* Your site config here */
   plugins: [],
 }
