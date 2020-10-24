632b092616a15177d7d9ae3b80153f42548cc652
mmmommm <ryota1839@gmail.com>
2020-10-24 12:45:51 +0900

    中身がとってこれないエラーを修正

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,7 +14,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
                 url
               }
             }
-            body {
+            text {
               childMarkdownRemark {
                 html
               }
